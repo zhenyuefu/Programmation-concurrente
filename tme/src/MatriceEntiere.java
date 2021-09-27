ab66b70b193ab7561acda0da5bb49524e94d2af9
Zhenyue Fu <zhenyue.fu@etu.sorbonne-universite.fr>
2021-09-27 20:29:31 +0200

    tme1完成

--- tme/src/MatriceEntiere.java
+++ tme/src/MatriceEntiere.java
@@ -1,6 +1,7 @@
-
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.util.Scanner;
-import java.io.*;
 
 public class MatriceEntiere {
     private int lig, col;
@@ -122,6 +123,16 @@ public class MatriceEntiere {
         MatriceEntiere matrice2 = new MatriceEntiere(file);
         try {
             MatriceEntiere result = matrice1.multipication(matrice2);
+            System.out.println(result);
+        } catch (TaillesNonConcordantesException e) {
+            e.printStackTrace();
+        }
+        file = new File("tme/data/donnees_somme1");
+        matrice1 = new MatriceEntiere(file);
+        file = new File("tme/data/donnees_somme2");
+        matrice2 = new MatriceEntiere(file);
+        try {
+            MatriceEntiere result = matrice1.add(matrice2);
             System.out.println(result);
         } catch (TaillesNonConcordantesException e) {
             e.printStackTrace();
